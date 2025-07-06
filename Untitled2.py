f35f912f39fd3300236f0bfb8ce6c50d14c316f9
Correctia <corrigetuexamen.ia@gmail.com>
2025-07-06 13:53:57 +0200

    Update Untitled2.py

--- Untitled2.py
+++ Untitled2.py
@@ -2410,7 +2410,6 @@ def show_settings():
     # Configuración de API
     st.subheader("🔑 Configuración")
 
-    ación de API
     st.subheader("🔑 Configuración de API")
     
     with st.form("api_config"):
